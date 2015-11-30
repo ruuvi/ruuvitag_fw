111188b8b525defb95a51d0d790ff4800be854bd
Lauri Jämsä <lauri@ruuvi.com>
2015-11-30 10:45:03 +0700

    Implemented Device Manager
    By following tutorial: https://devzone.nordicsemi.com/tutorials/15/

--- fw/main.c
+++ fw/main.c
@@ -40,6 +40,8 @@
 #include "softdevice_handler.h"
 #include "app_timer_appsh.h"
 #include "bsp.h"
+#include "device_manager.h"
+#include "pstorage.h"
 
 #define IS_SRVC_CHANGED_CHARACT_PRESENT 0                                           /**< Include or not the service_changed characteristic. if not enabled, the server's database cannot be changed for the lifetime of the device*/
 
@@ -313,79 +315,12 @@ static void on_adv_evt(ble_adv_evt_t ble_adv_evt)
 static void on_ble_evt(ble_evt_t * p_ble_evt)
 {
     uint32_t                         err_code;
-    static ble_gap_evt_auth_status_t m_auth_status;
-    bool                             master_id_matches;
-    ble_gap_sec_kdist_t *            p_distributed_keys;
-    ble_gap_enc_info_t *             p_enc_info;
-    ble_gap_irk_t *                  p_id_info;
-    ble_gap_sign_info_t *            p_sign_info;
-
-    static ble_gap_enc_key_t         m_enc_key;           /**< Encryption Key (Encryption Info and Master ID). */
-    static ble_gap_id_key_t          m_id_key;            /**< Identity Key (IRK and address). */
-    static ble_gap_sign_info_t       m_sign_key;          /**< Signing Key (Connection Signature Resolving Key). */
-    static ble_gap_sec_keyset_t      m_keys = {.keys_periph = {&m_enc_key, &m_id_key, &m_sign_key}};
-
     switch (p_ble_evt->header.evt_id)
     {
         case BLE_GAP_EVT_CONNECTED:
             err_code = bsp_indication_set(BSP_INDICATE_CONNECTED);
             APP_ERROR_CHECK(err_code);
             m_conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
-
-            /* YOUR_JOB: Uncomment this part if you are using the app_button module to handle button
-                         events (assuming that the button events are only needed in connected
-                         state). If this is uncommented out here,
-                            1. Make sure that app_button_disable() is called when handling
-                               BLE_GAP_EVT_DISCONNECTED below.
-                            2. Make sure the app_button module is initialized.
-            err_code = app_button_enable();
-            APP_ERROR_CHECK(err_code);
-            */
-            break;
-
-        case BLE_GAP_EVT_DISCONNECTED:
-            m_conn_handle = BLE_CONN_HANDLE_INVALID;
-
-            /* YOUR_JOB: Uncomment this part if you are using the app_button module to handle button
-                         events. This should be done to save power when not connected
-                         to a peer.
-            err_code = app_button_disable();
-            APP_ERROR_CHECK(err_code);
-            */
-            break;
-
-        case BLE_GAP_EVT_SEC_PARAMS_REQUEST:
-            err_code = sd_ble_gap_sec_params_reply(m_conn_handle,
-                                                   BLE_GAP_SEC_STATUS_SUCCESS,
-                                                   &m_sec_params,
-                                                   &m_keys);
-            APP_ERROR_CHECK(err_code);
-            break;
-
-        case BLE_GATTS_EVT_SYS_ATTR_MISSING:
-            err_code = sd_ble_gatts_sys_attr_set(m_conn_handle,
-                                                 NULL,
-                                                 0,
-                                                 BLE_GATTS_SYS_ATTR_FLAG_SYS_SRVCS | BLE_GATTS_SYS_ATTR_FLAG_USR_SRVCS);
-            APP_ERROR_CHECK(err_code);
-            break;
-
-        case BLE_GAP_EVT_AUTH_STATUS:
-            m_auth_status = p_ble_evt->evt.gap_evt.params.auth_status;
-            break;
-
-        case BLE_GAP_EVT_SEC_INFO_REQUEST:
-            master_id_matches  = memcmp(&p_ble_evt->evt.gap_evt.params.sec_info_request.master_id,
-                                        &m_enc_key.master_id,
-                                        sizeof(ble_gap_master_id_t)) == 0;
-            p_distributed_keys = &m_auth_status.kdist_periph;
-
-            p_enc_info  = (p_distributed_keys->enc  && master_id_matches) ? &m_enc_key.enc_info : NULL;
-            p_id_info   = (p_distributed_keys->id   && master_id_matches) ? &m_id_key.id_info   : NULL;
-            p_sign_info = (p_distributed_keys->sign && master_id_matches) ? &m_sign_key         : NULL;
-
-            err_code = sd_ble_gap_sec_info_reply(m_conn_handle, p_enc_info, p_id_info, p_sign_info);
-                APP_ERROR_CHECK(err_code);
             break;
 
         default:
@@ -412,6 +347,11 @@ static void ble_evt_dispatch(ble_evt_t * p_ble_evt)
     YOUR_JOB: Add service ble_evt handlers calls here, like, for example:
     ble_bas_on_ble_evt(&m_bas, p_ble_evt);
     */
+    
+    /* Added this line to register the device 
+    manger event handler with the BLE dispatcher: */
+
+    dm_ble_evt_handler(p_ble_evt); 
 }
 
 
@@ -424,6 +364,7 @@ static void ble_evt_dispatch(ble_evt_t * p_ble_evt)
  */
 static void sys_evt_dispatch(uint32_t sys_evt)
 {
+    pstorage_sys_event_handler(sys_evt); // This line is added to support Device Manager
     ble_advertising_on_sys_evt(sys_evt);
 }
 
@@ -546,13 +487,66 @@ static void bsp_module_init(void)
 }
 
 
+static dm_application_instance_t         m_app_handle;                              /**< Application identifier allocated by device manager */
+
+
+/**@brief Function for handling the Device Manager events.
+ *
+ * @param[in] p_evt  Data associated to the device manager event.
+ */
+static uint32_t device_manager_evt_handler(dm_handle_t const * p_handle,
+                                           dm_event_t const  * p_event,
+                                           ret_code_t        event_result)
+{
+    APP_ERROR_CHECK(event_result);
+
+    return NRF_SUCCESS;
+}
+
+
+/**@brief Function for the Device Manager initialization.
+ *
+ * @param[in] erase_bonds  Indicates whether bonding information should be cleared from
+ *                         persistent storage during initialization of the Device Manager.
+ */
+static void device_manager_init(bool erase_bonds)
+{
+    uint32_t               err_code;
+    dm_init_param_t        init_param = {.clear_persistent_data = erase_bonds};
+    dm_application_param_t register_param;
+
+    // Initialize persistent storage module.
+    err_code = pstorage_init();
+    APP_ERROR_CHECK(err_code);
+
+    err_code = dm_init(&init_param);
+    APP_ERROR_CHECK(err_code);
+
+    memset(&register_param.sec_param, 0, sizeof(ble_gap_sec_params_t));
+
+    register_param.sec_param.bond         = SEC_PARAM_BOND;
+    register_param.sec_param.mitm         = SEC_PARAM_MITM;
+    register_param.sec_param.io_caps      = SEC_PARAM_IO_CAPABILITIES;
+    register_param.sec_param.oob          = SEC_PARAM_OOB;
+    register_param.sec_param.min_key_size = SEC_PARAM_MIN_KEY_SIZE;
+    register_param.sec_param.max_key_size = SEC_PARAM_MAX_KEY_SIZE;
+    register_param.evt_handler            = device_manager_evt_handler;
+    register_param.service_type           = DM_PROTOCOL_CNTXT_GATT_SRVR_ID;
+
+    err_code = dm_register(&m_app_handle, &register_param);
+    APP_ERROR_CHECK(err_code);
+}
+
+
 /**@brief Function for application main entry.
  */
 int main(void)
 {
     uint32_t err_code;
+    bool erase_bonds = 0; // Set to 1 to erase all bonds
     
     // Initialize
+    device_manager_init(erase_bonds);
     timers_init();
     ble_stack_init();
     bsp_module_init();
@@ -562,6 +556,9 @@ int main(void)
     services_init();
     conn_params_init();
     sec_params_init();
+
+
+
 
     // Start execution
     timers_start();
