9330bceb1358db3778328d4a5d6ce7237c78443a
Lauri Jämsä <lauri@ruuvi.com>
2016-04-05 13:58:41 +0800

    New SDK requires these

--- board_configs/ruuvitag_b2.h
+++ board_configs/ruuvitag_b2.h
@@ -53,6 +53,9 @@
 #define SPIM0_SS_HUMI_PIN    3  // SPI Slave Select GPIO pin number. (BME280)
 
 // Low frequency clock source to be used by the SoftDevice
-#define NRF_CLOCK_LFCLKSRC      NRF_CLOCK_LFCLKSRC_XTAL_20_PPM
+#define NRF_CLOCK_LFCLKSRC      {.source        = NRF_CLOCK_LF_SRC_XTAL,            \
+                                 .rc_ctiv       = 0,                                \
+                                 .rc_temp_ctiv  = 0,                                \
+                                 .xtal_accuracy = NRF_CLOCK_LF_XTAL_ACCURACY_20_PPM}
 
 #endif // RUUVITAG_B2_H
