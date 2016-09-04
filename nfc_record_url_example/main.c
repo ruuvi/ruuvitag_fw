a6ca127597d43ecfffafe8b7ad0952d36aaa516c
Otso Jousimaa <otso@ojousima.net>
2016-09-04 19:48:11 +0300

    add SD12 support to NFC record url - untested

--- nfc_record_url_example/main.c
+++ nfc_record_url_example/main.c
@@ -23,32 +23,37 @@
 #include "nfc_uri_msg.h"
 #include "boards.h"
 #include "app_error.h"
+#include "hardfault.h"
+#include "nrf_log_ctrl.h"
 
 /** @snippet [NFC URI usage_0] */
-static const char url[] =
+static const uint8_t m_url[] =
     {'n', 'o', 'r', 'd', 'i', 'c', 's', 'e', 'm', 'i', '.', 'c', 'o', 'm'}; //URL "nordicsemi.com"
 
-uint8_t ndef_msg_buf[256];
+uint8_t m_ndef_msg_buf[256];
 /** @snippet [NFC URI usage_0] */
 /**
  * @brief Callback function for handling NFC events.
  */
-void nfc_callback(void *context, NfcEvent event, const char *data, size_t dataLength)
+static void nfc_callback(void * p_context, nfc_t2t_event_t event, const uint8_t * p_data, size_t data_length)
 {
-    (void)context;
+    (void)p_context;
 
     switch (event)
     {
-        case NFC_EVENT_FIELD_ON:
+        case NFC_T2T_EVENT_FIELD_ON:
             LEDS_ON(BSP_LED_0_MASK);
             break;
-        case NFC_EVENT_FIELD_OFF:
+
+        case NFC_T2T_EVENT_FIELD_OFF:
             LEDS_OFF(BSP_LED_0_MASK);
             break;
+
         default:
             break;
     }
 }
+
 
 /**
  * @brief Function for application main entry.
@@ -56,9 +61,8 @@ void nfc_callback(void *context, NfcEvent event, const char *data, size_t dataLe
 int main(void)
 {
     uint32_t  err_code;
-    NfcRetval ret_val;
-    
-    err_code = NRF_LOG_INIT();
+
+    err_code = NRF_LOG_INIT(NULL);
     APP_ERROR_CHECK(err_code);
 
     /* Configure LED-pins as outputs */
@@ -66,41 +70,35 @@ int main(void)
     LEDS_OFF(BSP_LED_0_MASK);
 
     /* Set up NFC */
-    ret_val = nfcSetup(nfc_callback, NULL);
-    if (ret_val != NFC_RETVAL_OK)
-    {
-        APP_ERROR_CHECK((uint32_t) ret_val);
-    }
+    err_code = nfc_t2t_setup(nfc_callback, NULL);
+    APP_ERROR_CHECK(err_code);
 
     /** @snippet [NFC URI usage_1] */
     /* Provide information about available buffer size to encoding function */
-    uint32_t len = sizeof(ndef_msg_buf);
+    uint32_t len = sizeof(m_ndef_msg_buf);
 
     /* Encode URI message into buffer */
     err_code = nfc_uri_msg_encode( NFC_URI_HTTP_WWW,
-                                   (uint8_t *) url,
-                                   sizeof(url),
-                                   ndef_msg_buf,
+                                   m_url,
+                                   sizeof(m_url),
+                                   m_ndef_msg_buf,
                                    &len);
 
     APP_ERROR_CHECK(err_code);
     /** @snippet [NFC URI usage_1] */
 
     /* Set created message as the NFC payload */
-    ret_val = nfcSetPayload( (char*)ndef_msg_buf, len);
-    if (ret_val != NFC_RETVAL_OK)
-    {
-        APP_ERROR_CHECK((uint32_t) ret_val);
-    }
+    err_code = nfc_t2t_payload_set(m_ndef_msg_buf, len);
+    APP_ERROR_CHECK(err_code);
 
     /* Start sensing NFC field */
-    ret_val = nfcStartEmulation();
-    if (ret_val != NFC_RETVAL_OK)
-    {
-        APP_ERROR_CHECK((uint32_t) ret_val);
-    }
+    err_code = nfc_t2t_emulation_start();
+    APP_ERROR_CHECK(err_code);
 
-    while(1){}
+    while (1)
+    {
+        NRF_LOG_FLUSH();
+    }
 }
 
 /** @} */
