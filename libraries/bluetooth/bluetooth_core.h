0bf01a2d9cfc13c9fe059e5a585da7a59077c3bd
Otso Jousimaa <otso@ojousima.net>
2017-04-08 13:00:46 -0500

    Rename ble driver constants to match SDK12.2

--- libraries/bluetooth/bluetooth_core.h
+++ libraries/bluetooth/bluetooth_core.h
@@ -8,9 +8,12 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include "ble_advdata.h"
+#include "bluetooth_config.h"
 #include "nordic_common.h"
 #include "softdevice_handler.h"
-
+#include "app_timer.h"
+#include "bsp.h"
 
 
 /**@brief Function for initializing the BLE stack.
@@ -29,38 +32,16 @@ uint32_t ble_stack_init(void);
  */
 uint32_t ble_tx_power_set(int8_t power);
 
-/**@brief Function for initializing the Advertising functionality.
+/**@brief Function for advertising data. 
  *
- * @details Encodes the required advertising data and passes it to the stack.
- *          Also builds a structure to be passed to the stack when starting advertising.
+ * @details Initializes the BLE advertisement with given data as manufacturer specific data.
+ * Companyt ID is included by default and doesn't need to be included.  
  *
- * @param m_beacon_info pointer to manufacturer specific data structure, for example:
- * uint8_t m_beacon_info[APP_BEACON_INFO_LENGTH] = 
- * {
- *   APP_DEVICE_TYPE,     // Manufacturer specific information. Specifies the device type in this
- *                        // implementation.
+ * @param data pointer to data to advertise, maximum length 24 bytes
+ * @param length length of data to advertise
  *
- *   APP_ADV_DATA_LENGTH, // Manufacturer specific information. Specifies the length of the
- *                        // manufacturer specific data in this implementation.
- *   APP_BEACON_UUID,     // 128 bit UUID value.
- *   APP_MAJOR_VALUE,     // Major arbitrary value that can be used to distinguish between Beacons.
- *   APP_MINOR_VALUE,     // Minor arbitrary value that can be used to distinguish between Beacons.
- *   APP_MEASURED_RSSI    // Manufacturer specific information. The Beacon's measured TX power in
- *                        // this implementation.
- *  };
- *
- * @param info_size size of manufacturer specific data to advertise
+ * @return error code from BLE stack initialization, NRF_SUCCESS if init was ok
  */
-void advertising_init(uint8_t *m_beacon_info, uint8_t info_size);
-
-/**@brief Function for starting advertising.
- *
- * @param connectable
- * BLE_GAP_ADV_TYPE_ADV_IND         //Connectable, undirected, not implemented
- * BLE_GAP_ADV_TYPE_ADV_DIRECT_IND  //Connectable, directed, not implemented
- * BLE_GAP_ADV_TYPE_ADV_SCAN_IND    //Scannable, undirected, not implemented
- * BLE_GAP_ADV_TYPE_ADV_NONCONN_IND //Non-connectable, undirected, not implemented
- */
-void advertising_start(uint8_t connectable);
+uint32_t bluetooth_advertise_data(uint8_t *data, uint8_t length);
 
 #endif
