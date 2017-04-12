e2b1ad175695fe97da21a760c45002619e83499d
Otso Jousimaa <otso@ojousima.net>
2017-04-12 12:05:04 -0500

    Update BLE config of template app

--- ruuvi_examples/template_app/ruuvitag_b3/s132/config/bluetooth_config.h
+++ ruuvi_examples/template_app/ruuvitag_b3/s132/config/bluetooth_config.h
@@ -13,19 +13,20 @@
 #ifndef BLUETOOTH_CONFIG_H
 #define BLUETOOTH_CONFIG_H 
 
-#define APP_COMPANY_IDENTIFIER              0xFFFF                                      //!< Test identifier, must be changed when in production
-#define APP_DEVICE_NAME                     "RuuviTag"                                  //!< Advertised device name in the scan response when in connectable mode.
-#define APP_BEACON_INFO_LENGTH              24                                          //!< Manufacturer specific data length. in bytes
+#define BLE_COMPANY_IDENTIFIER          0x0499                            /**< Ruuvi **/
+#define APP_DEVICE_NAME                 "RuuviTag"                        //!< Advertised device name in the scan response when in connectable mode.
 
-#define IS_SRVC_CHANGED_CHARACT_PRESENT 0                                 /**< Include the service changed characteristic. If not enabled, the server's database cannot be changed for the lifetime of the device. */
+#define APP_BEACON_INFO_LENGTH          24                                //!< Manufacturer specific data length. in bytes
+
+#define IS_SRVC_CHANGED_CHARACT_PRESENT 1                                 /**< Include the service changed characteristic. If not enabled, the server's database cannot be changed for the lifetime of the device. */
 
 #define CENTRAL_LINK_COUNT              0                                 /**< Number of central links used by the application. When changing this number remember to adjust the RAM settings*/
 #define PERIPHERAL_LINK_COUNT           0                                 /**< Number of peripheral links used by the application. When changing this number remember to adjust the RAM settings*/
 
 #define APP_CFG_NON_CONN_ADV_TIMEOUT    0                                 /**< Time for which the device must be advertising in non-connectable mode (in seconds). 0 disables the time-out. */
 
-#define APP_CFG_NON_CONN_ADV_INTERVAL_MS    MSEC_TO_UNITS(330, UNIT_0_625_MS) /**< The advertising interval for non-connectable advertisement (100 ms). This value can vary between 100 ms and 10.24 s). */
+#define APP_CFG_NON_CONN_ADV_INTERVAL_MS    MSEC_TO_UNITS(500, UNIT_0_625_MS) /**< The advertising interval for non-connectable advertisement (100 ms). This value can vary between 100 ms and 10.24 s). */
 
-#define BLE_TX_POWER                    4                                 /** dBm **/
+#define BLE_TX_POWER                    0                                 /** dBm **/
 
 #endif
