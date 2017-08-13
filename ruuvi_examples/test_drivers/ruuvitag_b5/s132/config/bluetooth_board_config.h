a24fa804d3d8b3b59634b6c23db3b690abb7526c
Otso Jousimaa <otso@ojousima.net>
2017-08-13 02:18:39 -0500

    Renaming board version to b5

---
+++ ruuvi_examples/test_drivers/ruuvitag_b5/s132/config/bluetooth_board_config.h
@@ -0,0 +1,9 @@
+#ifndef BLUETOOTH_BOARD_CONFIG_H
+#define BLUETOOTH_BOARD_CONFIG_H
+
+#define BLE_COMPANY_IDENTIFIER          0x0499                            /**< Ruuvi **/
+#define APP_DEVICE_NAME                 "RuuviTag"                        //!< Advertised device name in the scan response when in connectable mode.
+
+#define APP_BEACON_INFO_LENGTH          24                                //!< Manufacturer specific data length. in bytes
+
+#endif
