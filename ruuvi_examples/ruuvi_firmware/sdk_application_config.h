dc96a4de2c8a0f5f771e848445f6329c2e4b4a4e
Otso Jousimaa <otso@ojousima.net>
2017-10-13 18:13:54 +0300

    rename weather station to ruuvi firmware

---
+++ ruuvi_examples/ruuvi_firmware/sdk_application_config.h
@@ -0,0 +1,21 @@
+#ifndef SDK_APPLICATION_CONFIG_H
+#define SDK_APPLICATION_CONFIG_H
+
+//Define any application-specific sdk configuration overrides here
+#define TIMER_ENABLED 1
+#define PWM_ENABLED 1
+#define PWM0_ENABLED 1
+#define APP_PWM_ENABLED 1
+#define PPI_ENABLED 1
+#define TIMER_ENABLED 1
+#define TIMER0_ENABLED 1
+#define TIMER1_ENABLED 1
+#define TIMER2_ENABLED 1
+#define TIMER3_ENABLED 1
+#define TIMER4_ENABLED 1
+#define CRC16_ENABLED 1
+#define CRC32_ENABLED 1
+//#define NRF_LOG_ENABLED 0
+
+#endif
+
