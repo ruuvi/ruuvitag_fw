88521c9a44b253a25046f1ecd771492395498bf8
Otso Jousimaa <otso@ojousima.net>
2018-01-23 20:24:20 +0100

    Fix typo

--- ruuvi_examples/test_drivers/tests/test_nfc.c
+++ ruuvi_examples/test_drivers/tests/test_nfc.c
@@ -11,7 +11,7 @@ void test_nfc(void)
   //TODO: Test binary data setup
   //TODO: Setup limits for data, test error reporting from NFC.
   nfc_init(NULL, 0);
-  NRF_LOG_INFO("Wating for NFC field\r\n");
+  NRF_LOG_INFO("Waiting for NFC field\r\n");
   sd_app_evt_wait();
 
 }
