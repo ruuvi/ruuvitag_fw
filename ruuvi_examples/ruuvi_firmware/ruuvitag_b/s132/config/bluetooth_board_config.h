25cbe9dafb11a1804587421ca0fcfb1ad0b5d289
Otso Jousimaa <otso@ojousima.net>
2018-01-24 13:19:33 +0100

    Add DIS

--- ruuvi_examples/ruuvi_firmware/ruuvitag_b/s132/config/bluetooth_board_config.h
+++ ruuvi_examples/ruuvi_firmware/ruuvitag_b/s132/config/bluetooth_board_config.h
@@ -4,4 +4,12 @@
 #define BLE_COMPANY_IDENTIFIER              0x0499                                        //!< Ruuvi
 #define APP_CONFIG_CALIBRATED_RANGING_DATA  {-51, -41, -32, -26, -22, -17, -15, -10, -3}  //!< Calibrated TX power at 0 m. See the nRF52 Product Specification for corresponding TX values.
 
+/** Device Information Service initial values **/
+#define SERIAL_LENGTH                       9                                             /**< 2 UINT32_t hex-encoded + NULL */
+#define INIT_MANUFACTURER                   "Ruuvi Innovations Ltd"
+#define INIT_MODEL                          "RuuviTag"
+#define INIT_HWREV                          "B"
+#define INIT_FWREV                          "2.1.0-alpha"                                 /**< Github tag **/
+#define INIT_SWREV                          "2.X.Y"                                       /**< compatible library revisions should use this version **/
+
 #endif
