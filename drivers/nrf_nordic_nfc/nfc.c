9c50c0fe39cf9ba9394d060e286045b37e4972d7
Otso Jousimaa <otso@ojousima.net>
2018-02-20 13:52:17 +0800

    Fix #91

--- drivers/nrf_nordic_nfc/nfc.c
+++ drivers/nrf_nordic_nfc/nfc.c
@@ -7,7 +7,9 @@
 //#include "nfc_uri_msg.h" for URLs, remember to adjust makefile
 #include "boards.h"
 #include "app_error.h"
-#include "nrf_delay.h"
+//#include "nrf_delay.h"
+// SW_REV. TODO: refactor out of BLE config file
+#include "bluetooth_application_config.h"
 
 #define NRF_LOG_MODULE_NAME "NFC"
 #include "nrf_log.h"
@@ -18,7 +20,7 @@
 //Do not compile RAM-consuming buffers if they're not used
 #if NFC_HAL_ENABLED
 
-uint8_t m_ndef_msg_buf[256];
+uint8_t m_ndef_msg_buf[512];
 bool nfc_is_init = false;
 volatile bool field_on = false;
 nfc_callback_t app_callback = NULL;
@@ -76,11 +78,11 @@ ret_code_t nfc_init(uint8_t* data, uint32_t data_length)
     //Deinit NFC if it has been previously initialized
     if(nfc_is_init){
       NRF_LOG_INFO("Deinit NFC\r\n");
-      nrf_delay_ms(10);
+      //nrf_delay_ms(10);
       nfc_t2t_emulation_stop();
       nfc_t2t_done();
       NRF_LOG_INFO("Deinit NFC ok\r\n");
-      nrf_delay_ms(10);
+      //nrf_delay_ms(10);
     }
 
     /* Set up NFC */
@@ -90,6 +92,7 @@ ret_code_t nfc_init(uint8_t* data, uint32_t data_length)
     nfc_ndef_msg_clear(&NFC_NDEF_MSG(nfc_msg));
     id_record_add(&NFC_NDEF_MSG(nfc_msg));
     address_record_add(&NFC_NDEF_MSG(nfc_msg));
+    version_record_add(&NFC_NDEF_MSG(nfc_msg));
     data_record_add(&NFC_NDEF_MSG(nfc_msg), data, data_length);
 
     /* Encode welcome message */
@@ -111,29 +114,30 @@ ret_code_t nfc_init(uint8_t* data, uint32_t data_length)
 /**
  * Update NFC payload with given data. Data is converted to hex and printed as a string.
  * https://infocenter.nordicsemi.com/index.jsp?topic=%2Fcom.nordic.infocenter.sdk5.v12.0.0%2Fnfc_ndef_format_dox.html
+ * TODO: return err_code
  */
 void data_record_add(nfc_ndef_msg_desc_t* nfc_msg, uint8_t* data, uint32_t data_length)
 {
-  //uint32_t error_code = NRF_SUCCESS;
-  uint8_t empty[] = {0};
-  if(0 == data_length || NULL == data) { data = empty; data_length = 1;}
+  uint32_t error_code = NRF_SUCCESS;
+  uint8_t prefix[] = {'D', 'a', 't', 'a', ':'};
   //TODO: #define data length
-  static char data_string[60] = { 0 };
-  memset(data_string, 0, sizeof(data_string));
+  static char data_string[256] = { 0 };
+  memcpy(data_string, prefix, sizeof(prefix));
   for (uint8_t ii = 0; ii < data_length; ii++){
     sprintf(data_string+2*ii, "%02x", data[ii]);
   }
   uint8_t* data_bytes = (void*)&data_string;
-  static const uint8_t data_code[] = {'d', 't'};
+  const uint8_t data_code[] = {'d', 't'};
 
   NFC_NDEF_TEXT_RECORD_DESC_DEF(data_text_rec,
                                   UTF_8,
                                   data_code,
                                   sizeof(data_code),
                                   data_bytes,
-                                  data_length*2);
+                                  sizeof(prefix)+data_length*2);
    /** @snippet [NFC text usage_1] */
-  nfc_ndef_msg_record_add(nfc_msg, &NFC_NDEF_TEXT_RECORD_DESC(data_text_rec));
+  error_code = nfc_ndef_msg_record_add(nfc_msg, &NFC_NDEF_TEXT_RECORD_DESC(data_text_rec));
+  APP_ERROR_CHECK(error_code);
 }
 
 
@@ -145,20 +149,24 @@ void id_record_add(nfc_ndef_msg_desc_t* nfc_msg)
 {
     /** @snippet [NFC text usage_1] */
     uint32_t err_code = NRF_SUCCESS;
+    uint8_t prefix[] = {'I', 'D', ':', ' '};
+    static char id_string[30] = { 0 };
+    memcpy(id_string, prefix, sizeof(prefix));
     unsigned int id0 = NRF_FICR->DEVICEID[0];
     unsigned int id1 = NRF_FICR->DEVICEID[1];
-    //8 hex bytes
-    static char name[17] = { 0 };
-    sprintf(name, "%x%x", id0, id1);
-    uint8_t* name_bytes = (void*)&name;
-    static const uint8_t id_code[] = {'i', 'd'};
+    
+    sprintf(id_string + sizeof(prefix), "%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X", 
+                                        (id0>>24)%0xFF, (id0>>16)&0xFF, (id0>>8)%0xFF, id0%0xFF, 
+                                        (id1>>24)%0xFF, (id1>>16)%0xFF, (id1>>8)%0xFF, id1&0xFF);
+    uint8_t* name_bytes = (void*)&id_string;
+    const uint8_t id_code[] = {'i', 'd'};
 
     NFC_NDEF_TEXT_RECORD_DESC_DEF(id_text_rec,
                                   UTF_8,
                                   id_code,
                                   sizeof(id_code),
                                   name_bytes,
-                                  sizeof(name));
+                                  sizeof(id_string));
    /** @snippet [NFC text usage_1] */
     err_code = nfc_ndef_msg_record_add(nfc_msg,
                                        &NFC_NDEF_TEXT_RECORD_DESC(id_text_rec));
@@ -181,12 +189,12 @@ void address_record_add(nfc_ndef_msg_desc_t* nfc_msg)
     mac_buffer[4] = ((NRF_FICR->DEVICEADDR[0]>>8)&0xFF);
     mac_buffer[5] = ((NRF_FICR->DEVICEADDR[0]>>0)&0xFF);
     //8 hex bytes
-    static char name[17] = { 0 };
-    sprintf(name, "%02x%02x%02x%02x%02x%02x", mac_buffer[0], mac_buffer[1], mac_buffer[2], mac_buffer[3], mac_buffer[4], mac_buffer[5]);
+    static char name[30] = { 0 };
+    sprintf(name, "MAC: %02X:%02X:%02X:%02X:%02X:%02X", mac_buffer[0], mac_buffer[1], mac_buffer[2], mac_buffer[3], mac_buffer[4], mac_buffer[5]);
     uint8_t* name_bytes = (void*)&name;
-    static const uint8_t addr_code[] = {'a', 'd'};
+    const uint8_t addr_code[] = {'a', 'd'};
 
-    NFC_NDEF_TEXT_RECORD_DESC_DEF(id_text_rec,
+    NFC_NDEF_TEXT_RECORD_DESC_DEF(addr_text_rec,
                                   UTF_8,
                                   addr_code,
                                   sizeof(addr_code),
@@ -194,7 +202,33 @@ void address_record_add(nfc_ndef_msg_desc_t* nfc_msg)
                                   sizeof(name));
    /** @snippet [NFC text usage_1] */
     err_code = nfc_ndef_msg_record_add(nfc_msg,
-                                       &NFC_NDEF_TEXT_RECORD_DESC(id_text_rec));
+                                       &NFC_NDEF_TEXT_RECORD_DESC(addr_text_rec));
+    APP_ERROR_CHECK(err_code);
+}
+
+/**
+ * @brief Function for creating a record.
+ */
+void version_record_add(nfc_ndef_msg_desc_t* nfc_msg)
+{
+    /** @snippet [NFC text usage_1] */
+    uint32_t err_code = NRF_SUCCESS;
+    uint8_t prefix[] = {'S', 'W', ':', ' '};
+    static char name[30] = { 0 };
+    memcpy(name, prefix, sizeof(prefix));
+    memcpy(name + sizeof(prefix), INIT_SWREV, sizeof(INIT_SWREV));
+    uint8_t* name_bytes = (void*)&name;
+    const uint8_t version_code[] = {'s', 'w'};
+
+    NFC_NDEF_TEXT_RECORD_DESC_DEF(version_text_rec,
+                                  UTF_8,
+                                  version_code,
+                                  sizeof(version_code),
+                                  name_bytes,
+                                  sizeof(name));
+   /** @snippet [NFC text usage_1] */
+    err_code = nfc_ndef_msg_record_add(nfc_msg,
+                                       &NFC_NDEF_TEXT_RECORD_DESC(version_text_rec));
     APP_ERROR_CHECK(err_code);
 }
 
