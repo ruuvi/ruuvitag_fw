c9a1dce2bd506ad0d5d24145f42d46d53c2978d8
Otso Jousimaa <otso@ojousima.net>
2018-07-18 16:25:36 +0300

    Fix indentation

--- drivers/nrf_nordic_pininterrupt/pin_interrupt.c
+++ drivers/nrf_nordic_pininterrupt/pin_interrupt.c
@@ -17,19 +17,19 @@
  */
 ret_code_t pin_interrupt_init()
 {
-    ret_code_t err_code = NRF_SUCCESS;
-    err_code |= nrf_drv_gpiote_init();
-    return err_code;
+  ret_code_t err_code = NRF_SUCCESS;
+  err_code |= nrf_drv_gpiote_init();
+  return err_code;
 }
 
 //Look-up table for event handlers
 static message_handler pin_event_handlers[32] = {0};
 static void in_pin_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
 {
-	//Call event handler with empty message TODO: invalid message add context?
-	NRF_LOG_DEBUG("Handling pin event\r\n");
-	ruuvi_standard_message_t message;
-    if(NULL != pin_event_handlers[pin]){ (pin_event_handlers[pin])(message);}
+  //Call event handler with empty message TODO: invalid message add context?
+  NRF_LOG_DEBUG("Handling pin event\r\n");
+  ruuvi_standard_message_t message;
+  if (NULL != pin_event_handlers[pin]) { (pin_event_handlers[pin])(message);}
 }
 /**
  *  Enable interrput on pin. Pull-up is enabled on HITOLOW, pull-down is enabled on LOWTIHI
@@ -38,37 +38,37 @@ static void in_pin_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t actio
  *  NRF_GPIOTE_POLARITY_HITOLO
  *  NRF_GPIOTE_POLARITY_TOGGLE
  *
- *  Message handler is called with an empty message on event. 
+ *  Message handler is called with an empty message on event.
  */
 ret_code_t pin_interrupt_enable(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t polarity, message_handler handler)
 {
-	NRF_LOG_INFO("Enabling\r\n");
-	ret_code_t err_code = NRF_SUCCESS;
-	nrf_drv_gpiote_in_config_t in_config = {                                            \
-        .is_watcher = false,               \
-        .hi_accuracy = false,              \
-        .pull = NRF_GPIO_PIN_NOPULL,       \
-        .sense = polarity                  \
-    };
-	switch(polarity)
-	{
-	  case NRF_GPIOTE_POLARITY_TOGGLE:
-          in_config.pull = NRF_GPIO_PIN_NOPULL;
-	      break;
-	  case NRF_GPIOTE_POLARITY_HITOLO:
-	      NRF_LOG_INFO("Pull-up\r\n");
-	      in_config.pull = NRF_GPIO_PIN_PULLUP;
-	      break;
-	  case NRF_GPIOTE_POLARITY_LOTOHI:
-	      in_config.pull = NRF_GPIO_PIN_PULLDOWN;
-	      break;
-	   default:
-	      return 1; //TODO proper error code
-    }
-    pin_event_handlers[pin] = handler;
-	err_code |= nrf_drv_gpiote_in_init(pin, &in_config, in_pin_handler);
+  NRF_LOG_INFO("Enabling\r\n");
+  ret_code_t err_code = NRF_SUCCESS;
+  nrf_drv_gpiote_in_config_t in_config = {
+    .is_watcher = false,               \
+    .hi_accuracy = false,              \
+    .pull = NRF_GPIO_PIN_NOPULL,       \
+    .sense = polarity                  \
+  };
+  switch (polarity)
+  {
+  case NRF_GPIOTE_POLARITY_TOGGLE:
+    in_config.pull = NRF_GPIO_PIN_NOPULL;
+    break;
+  case NRF_GPIOTE_POLARITY_HITOLO:
+    NRF_LOG_INFO("Pull-up\r\n");
+    in_config.pull = NRF_GPIO_PIN_PULLUP;
+    break;
+  case NRF_GPIOTE_POLARITY_LOTOHI:
+    in_config.pull = NRF_GPIO_PIN_PULLDOWN;
+    break;
+  default:
+    return 1; //TODO proper error code
+  }
+  pin_event_handlers[pin] = handler;
+  err_code |= nrf_drv_gpiote_in_init(pin, &in_config, in_pin_handler);
 
-	nrf_drv_gpiote_in_event_enable(pin, true);
+  nrf_drv_gpiote_in_event_enable(pin, true);
 
-	return err_code;
+  return err_code;
 }
