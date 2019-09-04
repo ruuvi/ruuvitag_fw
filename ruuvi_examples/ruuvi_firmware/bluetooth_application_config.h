3c474370b679e91a943ecac47e844eb291edd223
Otso Jousimaa <otso@ojousima.net>
2019-09-04 11:31:35 +0300

    update version

--- ruuvi_examples/ruuvi_firmware/bluetooth_application_config.h
+++ ruuvi_examples/ruuvi_firmware/bluetooth_application_config.h
@@ -1,11 +1,11 @@
 #ifndef BLUETOOTH_APP_CONFIG
 #define BLUETOOTH_APP_CONFIG
-#define APPLICATION_DEVICE_NAME         "Ruuvi-nobme"                         /**< BLE name displayed in scan response. */
+#define APPLICATION_DEVICE_NAME         "Ruuvi"                         /**< BLE name displayed in scan response. */
 #define APPLICATION_DEVICE_NAME_LENGTH  5                               /**< number of characters in above string, excluding null */
 #define APP_DEVICE_NAME                 APPLICATION_DEVICE_NAME         /**< TODO: Refactoring **/
 #define APP_DEVICE_NAME_LENGTH          APPLICATION_DEVICE_NAME_LENGTH
 #define APP_TX_POWER                    4                               /**< dBm **/
-#define INIT_FWREV                      "2.5.4"                         /**< Github tag. Do not include specifiers such as "alpha" so you can accept ready binaries as they are **/
+#define INIT_FWREV                      "2.5.5"                         /**< Github tag. Do not include specifiers such as "alpha" so you can accept ready binaries as they are **/
 #define INIT_SWREV                      INIT_FWREV                      /**< FW and SW are same thing in this context **/                             
 
 // milliseconds until main loop timer function is called. Other timers can bring
